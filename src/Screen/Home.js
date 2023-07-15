4e10cbf7902064ba15f350bfb869e5311e9e434a
harshit1142 <harshitpandey98765@gmail.com>
2023-07-15 18:49:07 +0530

    -Initial Setup

---
+++ src/Screen/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function 
+() {
+  return (
+    <div className='container-fluid home d-flex flex-column align-items-center'>
+        <div className='container-fluid d-flex justify-content-center w-100 '>
+           <div className='logo'>Tic Tac Toe</div>
+        </div>
+        <div className='ttt'>
+            
+        </div>
+    </div>
+  )
+}
